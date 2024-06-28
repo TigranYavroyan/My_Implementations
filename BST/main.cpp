6e31d7f8f6b45888ba5098890bfc82e778e605f1
tigran <tyavroyan@gmail.com>
2024-06-28 20:10:47 +0400

    del

--- BST/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "BST.hpp"
-
-int main () {
-    std::initializer_list il = {2, 1, -2, -5, -1, 5, 4, 8, 7, 9};
-    BST<int> tree = il;
-    tree.postorder([](const int val){std::cout << val << ' ';});
-    std::cout << '\n';
-    std::cout << tree.predecessor(1) << std::endl;
-}
